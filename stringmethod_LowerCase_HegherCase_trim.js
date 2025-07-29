de6537eb89c88ae5e6ccef79646ae3a0b8d2663b
RENNYAWATANABE <127462554+RENNYAWATANABE@users.noreply.github.com>
2025-07-29 14:23:55 +0900

    Add files via upload

---
+++ stringmethod_LowerCase_HegherCase_trim.js
@@ -0,0 +1,23 @@
+
+//UpperCase(Stringメソッド)  テスト
+let hello = "hello world!!";
+console.log(hello);
+console.log(hello.toUpperCase());
+
+
+//LowerCase(Stringメソッド) テスト  &trim(Stringメソッド)
+let test = "    TEST XXXXXX     ";
+console.log(test);
+console.log(test.trim().toLowerCase());
+
+
+
+//練習１
+
+
+
+
+
+
+
+
